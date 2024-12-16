7f5f32aba2321eb49309055304550a4b79f57afb
pchchv <ipchchv@gmail.com>
2024-12-16 09:59:08 +0300

    Create type Jwk

---
+++ src/oauth/jwk.go
@@ -0,0 +1,10 @@
+package oauth
+
+type Jwk struct {
+	Alg string `json:"alg"`
+	Kid string `json:"kid"`
+	Kty string `json:"kty"`
+	Use string `json:"use"`
+	N   string `json:"n"`
+	E   string `json:"e"`
+}
