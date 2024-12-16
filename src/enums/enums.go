3f7a3a5168117835322dfa15d8a5ae213775a713
pchchv <ipchchv@gmail.com>
2024-12-16 10:18:36 +0300

    Create an AcrLevel type

--- src/enums/enums.go
+++ src/enums/enums.go
@@ -4,6 +4,10 @@ const (
 	TokenTypeId TokenType = iota
 	TokenTypeBearer
 	TokenTypeRefresh
+
+	AcrLevel1          AcrLevel = "urn:goiabada:level1"
+	AcrLevel2Optional  AcrLevel = "urn:goiabada:level2_optional"
+	AcrLevel2Mandatory AcrLevel = "urn:goiabada:level2_mandatory"
 )
 
 type TokenType int
@@ -11,3 +15,5 @@ type TokenType int
 func (tt TokenType) String() string {
 	return []string{"ID", "Bearer", "Refresh"}[tt]
 }
+
+type AcrLevel string
