02d80a39559de07351bd395c2c6ab88e6516038f
pchchv <ipchchv@gmail.com>
2024-12-16 10:07:53 +0300

    Implement a GetAddressClaim and a GetStringClaim methods

--- src/oauth/jwt.go
+++ src/oauth/jwt.go
@@ -6,3 +6,23 @@ type Jwt struct {
 	TokenBase64 string
 	Claims      jwt.MapClaims
 }
+
+func (jwt Jwt) GetAddressClaim() map[string]string {
+	if jwt.Claims["address"] != nil {
+		if addressMap, ok := jwt.Claims["address"].(map[string]interface{}); ok {
+			result := make(map[string]string)
+			for k, v := range addressMap {
+				result[k] = v.(string)
+			}
+			return result
+		}
+	}
+	return map[string]string{}
+}
+
+func (jwt Jwt) GetStringClaim(claimName string) string {
+	if jwt.Claims[claimName] != nil {
+		return jwt.Claims[claimName].(string)
+	}
+	return ""
+}
