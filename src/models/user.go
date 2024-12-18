512dd7321822368f4dbee4c071a65e0d36577513
pchchv <ipchchv@gmail.com>
2024-12-18 11:29:47 +0300

    Create an User type

---
+++ src/models/user.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id                                   int64           `db:"id" fieldtag:"pk"`
+	CreatedAt                            sql.NullTime    `db:"created_at" fieldtag:"dont-update"`
+	UpdatedAt                            sql.NullTime    `db:"updated_at"`
+	Enabled                              bool            `db:"enabled"`
+	Subject                              uuid.UUID       `db:"subject"`
+	Username                             string          `db:"username"`
+	GivenName                            string          `db:"given_name"`
+	MiddleName                           string          `db:"middle_name"`
+	FamilyName                           string          `db:"family_name"`
+	Nickname                             string          `db:"nickname"`
+	Website                              string          `db:"website"`
+	Gender                               string          `db:"gender"`
+	Email                                string          `db:"email"`
+	EmailVerified                        bool            `db:"email_verified"`
+	EmailVerificationCodeEncrypted       []byte          `db:"email_verification_code_encrypted"`
+	EmailVerificationCodeIssuedAt        sql.NullTime    `db:"email_verification_code_issued_at"`
+	ZoneInfoCountryName                  string          `db:"zone_info_country_name"`
+	ZoneInfo                             string          `db:"zone_info"`
+	Locale                               string          `db:"locale"`
+	BirthDate                            sql.NullTime    `db:"birth_date"`
+	PhoneNumberCountryUniqueId           string          `db:"phone_number_country_uniqueid"`
+	PhoneNumberCountryCallingCode        string          `db:"phone_number_country_callingcode"`
+	PhoneNumber                          string          `db:"phone_number"`
+	PhoneNumberVerified                  bool            `db:"phone_number_verified"`
+	PhoneNumberVerificationCodeEncrypted []byte          `db:"phone_number_verification_code_encrypted"`
+	PhoneNumberVerificationCodeIssuedAt  sql.NullTime    `db:"phone_number_verification_code_issued_at"`
+	AddressLine1                         string          `db:"address_line1"`
+	AddressLine2                         string          `db:"address_line2"`
+	AddressLocality                      string          `db:"address_locality"`
+	AddressRegion                        string          `db:"address_region"`
+	AddressPostalCode                    string          `db:"address_postal_code"`
+	AddressCountry                       string          `db:"address_country"`
+	PasswordHash                         string          `db:"password_hash"`
+	OTPSecret                            string          `db:"otp_secret"`
+	OTPEnabled                           bool            `db:"otp_enabled"`
+	ForgotPasswordCodeEncrypted          []byte          `db:"forgot_password_code_encrypted"`
+	ForgotPasswordCodeIssuedAt           sql.NullTime    `db:"forgot_password_code_issued_at"`
+	Groups                               []Group         `db:"-"`
+	Permissions                          []Permission    `db:"-"`
+	Attributes                           []UserAttribute `db:"-"`
+}
