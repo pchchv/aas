a71d3f89dfacbcc9f88f92e780a699f431bccc07
pchchv <ipchchv@gmail.com>
2024-12-20 13:19:41 +0300

    Implement an encoding functions

--- src/rsautil/rsautil.go
+++ src/rsautil/rsautil.go
@@ -5,3 +5,32 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 )
+
+func EncodePrivateKeyToPEM(privateKey *rsa.PrivateKey) (privatePEM []byte) {
+	// ASN.1 DER format
+	privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+	// pem.Block
+	privBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privDER,
+	}
+	privatePEM = pem.EncodeToMemory(&privBlock)
+
+	return
+}
+
+func EncodePublicKeyToPEM(publicKey *rsa.PublicKey) (pubkey_pem []byte, err error) {
+	if pubkey_bytes, err := x509.MarshalPKIXPublicKey(publicKey); err != nil {
+		return nil, err
+	} else {
+		pubkey_pem = pem.EncodeToMemory(
+			&pem.Block{
+				Type:  "RSA PUBLIC KEY",
+				Bytes: pubkey_bytes,
+			},
+		)
+	}
+
+	return
+}
