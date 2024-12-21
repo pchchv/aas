079e6f05a5c57f9a58743ddc43262b45959b2b2e
pchchv <ipchchv@gmail.com>
2024-12-21 04:14:52 +0300

    Implement a CreateSettings method

--- src/data/commondb/settings.go
+++ src/data/commondb/settings.go
@@ -2,6 +2,7 @@ package commondb
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/huandu/go-sqlbuilder"
 	"github.com/pchchv/aas/src/models"
@@ -14,6 +15,33 @@ func (d *CommonDB) GetSettingsById(tx *sql.Tx, settingsId int64) (*models.Settin
 	selectBuilder.Where(selectBuilder.Equal("id", settingsId))
 
 	return d.getSettingsCommon(tx, selectBuilder, settingsStruct)
+}
+
+func (d *CommonDB) CreateSettings(tx *sql.Tx, settings *models.Settings) error {
+	now := time.Now().UTC()
+	originalCreatedAt := settings.CreatedAt
+	originalUpdatedAt := settings.UpdatedAt
+	settings.CreatedAt = sql.NullTime{Time: now, Valid: true}
+	settings.UpdatedAt = sql.NullTime{Time: now, Valid: true}
+	settingsStruct := sqlbuilder.NewStruct(new(models.Settings)).For(d.Flavor)
+	insertBuilder := settingsStruct.WithoutTag("pk").InsertInto("settings", settings)
+	sql, args := insertBuilder.Build()
+	result, err := d.ExecSql(tx, sql, args...)
+	if err != nil {
+		settings.CreatedAt = originalCreatedAt
+		settings.UpdatedAt = originalUpdatedAt
+		return errors.Wrap(err, "unable to insert settings")
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		settings.CreatedAt = originalCreatedAt
+		settings.UpdatedAt = originalUpdatedAt
+		return errors.Wrap(err, "unable to get last insert id")
+	}
+
+	settings.Id = id
+	return nil
 }
 
 func (d *CommonDB) getSettingsCommon(tx *sql.Tx, selectBuilder *sqlbuilder.SelectBuilder, settingsStruct *sqlbuilder.Struct) (*models.Settings, error) {
