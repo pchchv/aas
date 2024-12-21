f66ac38a147a9ca02da7eee8a32869d091e2c007
pchchv <ipchchv@gmail.com>
2024-12-21 03:51:19 +0300

    Implement a method Log

--- src/data/commondb/db.go
+++ src/data/commondb/db.go
@@ -2,8 +2,11 @@ package commondb
 
 import (
 	"database/sql"
+	"fmt"
+	"log/slog"
 
 	"github.com/huandu/go-sqlbuilder"
+	"github.com/pchchv/aas/src/config"
 )
 
 type CommonDB struct {
@@ -15,5 +18,16 @@ func NewCommonDB(db *sql.DB, flavor sqlbuilder.Flavor) *CommonDB {
 	return &CommonDB{
 		DB:     db,
 		Flavor: flavor,
+	}
+}
+
+func (d *CommonDB) Log(sql string, args ...any) {
+	if config.Get().LogSQL {
+		var argsStr string
+		slog.Info(fmt.Sprintf("sql: %v", sql))
+		for i, arg := range args {
+			argsStr += fmt.Sprintf("[arg %v: %v] ", i, arg)
+		}
+		slog.Info(fmt.Sprintf("sql args: %v", argsStr))
 	}
 }
