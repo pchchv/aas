f43e22c5f4b90f722f24e3e0fd797240d273c29c
pchchv <ipchchv@gmail.com>
2025-01-12 15:50:47 +0300

    GetPermissionsByResourceId&GetPermissionsByIds methods

--- src/data/commondb/permission.go
+++ src/data/commondb/permission.go
@@ -39,3 +39,55 @@ func (d *CommonDB) CreatePermission(tx *sql.Tx, permission *models.Permission) e
 	permission.Id = id
 	return nil
 }
+
+func (d *CommonDB) GetPermissionsByResourceId(tx *sql.Tx, resourceId int64) (permissions []models.Permission, err error) {
+	permissionStruct := sqlbuilder.NewStruct(new(models.Permission)).For(d.Flavor)
+	selectBuilder := permissionStruct.SelectFrom("permissions")
+	selectBuilder.Where(selectBuilder.Equal("resource_id", resourceId))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var permission models.Permission
+		addr := permissionStruct.Addr(&permission)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan permission")
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	return permissions, nil
+}
+
+func (d *CommonDB) GetPermissionsByIds(tx *sql.Tx, permissionIds []int64) (permissions []models.Permission, err error) {
+	if len(permissionIds) == 0 {
+		return
+	}
+
+	permissionStruct := sqlbuilder.NewStruct(new(models.Permission)).For(d.Flavor)
+	selectBuilder := permissionStruct.SelectFrom("permissions")
+	selectBuilder.Where(selectBuilder.In("id", sqlbuilder.Flatten(permissionIds)...))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var permission models.Permission
+		addr := permissionStruct.Addr(&permission)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan permission")
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	return permissions, nil
+}
