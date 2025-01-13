277a35502937e8edd0caa013f82a8e8c3ffacade
pchchv <ipchchv@gmail.com>
2025-01-13 20:02:43 +0300

    Implement a CreateUser method

--- src/data/commondb/user.go
+++ src/data/commondb/user.go
@@ -2,11 +2,39 @@ package commondb
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/huandu/go-sqlbuilder"
 	"github.com/pchchv/aas/src/models"
 	"github.com/pkg/errors"
 )
+
+func (d *CommonDB) CreateUser(tx *sql.Tx, user *models.User) error {
+	now := time.Now().UTC()
+	originalCreatedAt := user.CreatedAt
+	originalUpdatedAt := user.UpdatedAt
+	user.CreatedAt = sql.NullTime{Time: now, Valid: true}
+	user.UpdatedAt = sql.NullTime{Time: now, Valid: true}
+	userStruct := sqlbuilder.NewStruct(new(models.User)).For(d.Flavor)
+	insertBuilder := userStruct.WithoutTag("pk").InsertInto("users", user)
+	sql, args := insertBuilder.Build()
+	result, err := d.ExecSql(tx, sql, args...)
+	if err != nil {
+		user.CreatedAt = originalCreatedAt
+		user.UpdatedAt = originalUpdatedAt
+		return errors.Wrap(err, "unable to insert user")
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		user.CreatedAt = originalCreatedAt
+		user.UpdatedAt = originalUpdatedAt
+		return errors.Wrap(err, "unable to get last insert id")
+	}
+
+	user.Id = id
+	return nil
+}
 
 func (d *CommonDB) getUserCommon(tx *sql.Tx, selectBuilder *sqlbuilder.SelectBuilder, userStruct *sqlbuilder.Struct) (*models.User, error) {
 	sql, args := selectBuilder.Build()
