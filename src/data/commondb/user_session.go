294db07328990b1afa5e05bb6b1865787b4bc54b
pchchv <ipchchv@gmail.com>
2025-01-15 10:53:46 +0300

    Implement a method GetUserSessionsByClientIdPaginated

--- src/data/commondb/user_session.go
+++ src/data/commondb/user_session.go
@@ -39,3 +39,61 @@ func (d *CommonDB) CreateUserSession(tx *sql.Tx, userSession *models.UserSession
 
 	return nil
 }
+
+func (d *CommonDB) GetUserSessionsByClientIdPaginated(tx *sql.Tx, clientId int64, page int, pageSize int) (userSessions []models.UserSession, total int, err error) {
+	if clientId <= 0 {
+		return nil, 0, errors.WithStack(errors.New("client id must be greater than 0"))
+	}
+
+	if page < 1 {
+		page = 1
+	}
+
+	if pageSize < 1 {
+		pageSize = 10
+	}
+
+	userSessionStruct := sqlbuilder.NewStruct(new(models.UserSession)).For(d.Flavor)
+	selectBuilder := userSessionStruct.SelectFrom("user_sessions")
+	selectBuilder.JoinWithOption(sqlbuilder.InnerJoin, "user_session_clients", "user_sessions.id = user_session_clients.user_session_id")
+	selectBuilder.Where(selectBuilder.Equal("user_session_clients.client_id", clientId))
+	selectBuilder.OrderBy("user_sessions.last_accessed").Desc()
+	selectBuilder.Offset((page - 1) * pageSize)
+	selectBuilder.Limit(pageSize)
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, 0, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userSession models.UserSession
+		addr := userSessionStruct.Addr(&userSession)
+		err = rows.Scan(addr...)
+		if err != nil {
+			return nil, 0, errors.Wrap(err, "unable to scan userSession")
+		}
+		userSessions = append(userSessions, userSession)
+	}
+
+	selectBuilder = d.Flavor.NewSelectBuilder()
+	selectBuilder.Select("count(*)").From("user_sessions")
+	selectBuilder.JoinWithOption(sqlbuilder.InnerJoin, "user_session_clients", "user_sessions.id = user_session_clients.user_session_id")
+	selectBuilder.Where(selectBuilder.Equal("user_session_clients.client_id", clientId))
+
+	sql, args = selectBuilder.Build()
+	rows2, err := d.QuerySql(nil, sql, args...)
+	if err != nil {
+		return nil, 0, errors.Wrap(err, "unable to query database")
+	}
+	defer rows2.Close()
+
+	if rows2.Next() {
+		if err = rows2.Scan(&total); err != nil {
+			return nil, 0, errors.Wrap(err, "unable to scan total")
+		}
+	}
+
+	return
+}
