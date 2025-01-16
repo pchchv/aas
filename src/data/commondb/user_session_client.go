afd461c321ecaabfada62cf7dfa2ae0055f4b19b
pchchv <ipchchv@gmail.com>
2025-01-16 09:47:51 +0300

    GetUserSessionClientById&GetUserSessionClientsByUserSessionId methods

--- src/data/commondb/user_session_client.go
+++ src/data/commondb/user_session_client.go
@@ -90,6 +90,36 @@ func (d *CommonDB) GetUserSessionClientsByUserSessionIds(tx *sql.Tx, userSession
 	return
 }
 
+func (d *CommonDB) GetUserSessionClientById(tx *sql.Tx, userSessionClientId int64) (*models.UserSessionClient, error) {
+	userSessionClientStruct := sqlbuilder.NewStruct(new(models.UserSessionClient)).For(d.Flavor)
+	selectBuilder := userSessionClientStruct.SelectFrom("user_session_clients")
+	selectBuilder.Where(selectBuilder.Equal("id", userSessionClientId))
+	return d.getUserSessionClientCommon(tx, selectBuilder, userSessionClientStruct)
+}
+
+func (d *CommonDB) GetUserSessionClientsByUserSessionId(tx *sql.Tx, userSessionId int64) (userSessionClients []models.UserSessionClient, err error) {
+	userSessionClientStruct := sqlbuilder.NewStruct(new(models.UserSessionClient)).For(d.Flavor)
+	selectBuilder := userSessionClientStruct.SelectFrom("user_session_clients")
+	selectBuilder.Where(selectBuilder.Equal("user_session_id", userSessionId))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userSessionClient models.UserSessionClient
+		addr := userSessionClientStruct.Addr(&userSessionClient)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan userSessionClient")
+		}
+		userSessionClients = append(userSessionClients, userSessionClient)
+	}
+
+	return
+}
+
 func (d *CommonDB) getUserSessionClientCommon(tx *sql.Tx, selectBuilder *sqlbuilder.SelectBuilder, userSessionClientStruct *sqlbuilder.Struct) (*models.UserSessionClient, error) {
 	sql, args := selectBuilder.Build()
 	rows, err := d.QuerySql(tx, sql, args...)
