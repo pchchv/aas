134f0fa50e9f519f235e45eb8ad680c2a6e34d18
pchchv <ipchchv@gmail.com>
2025-01-16 10:10:12 +0300

    Implement a CreateGroup method

---
+++ src/data/commondb/group.go
@@ -0,0 +1,37 @@
+package commondb
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/huandu/go-sqlbuilder"
+	"github.com/pchchv/aas/src/models"
+	"github.com/pkg/errors"
+)
+
+func (d *CommonDB) CreateGroup(tx *sql.Tx, group *models.Group) error {
+	now := time.Now().UTC()
+	originalCreatedAt := group.CreatedAt
+	originalUpdatedAt := group.UpdatedAt
+	group.CreatedAt = sql.NullTime{Time: now, Valid: true}
+	group.UpdatedAt = sql.NullTime{Time: now, Valid: true}
+	groupStruct := sqlbuilder.NewStruct(new(models.Group)).For(d.Flavor)
+	insertBuilder := groupStruct.WithoutTag("pk").InsertInto(d.Flavor.Quote("groups"), group)
+	sql, args := insertBuilder.Build()
+	result, err := d.ExecSql(tx, sql, args...)
+	if err != nil {
+		group.CreatedAt = originalCreatedAt
+		group.UpdatedAt = originalUpdatedAt
+		return errors.Wrap(err, "unable to insert group")
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		group.CreatedAt = originalCreatedAt
+		group.UpdatedAt = originalUpdatedAt
+		return errors.Wrap(err, "unable to get last insert id")
+	}
+
+	group.Id = id
+	return nil
+}
