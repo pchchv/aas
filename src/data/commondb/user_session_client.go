e0204371b5a95ee593ac7574746f6cdf698498e9
pchchv <ipchchv@gmail.com>
2025-01-16 09:44:44 +0300

    GetUserSessionsClientByIds&GetUserSessionClientsByUserSessionIds methods

--- src/data/commondb/user_session_client.go
+++ src/data/commondb/user_session_client.go
@@ -35,3 +35,57 @@ func (d *CommonDB) CreateUserSessionClient(tx *sql.Tx, userSessionClient *models
 	userSessionClient.Id = id
 	return nil
 }
+
+func (d *CommonDB) GetUserSessionsClientByIds(tx *sql.Tx, userSessionClientIds []int64) (userSessionClients []models.UserSessionClient, err error) {
+	if len(userSessionClientIds) == 0 {
+		return nil, nil
+	}
+
+	userSessionClientStruct := sqlbuilder.NewStruct(new(models.UserSessionClient)).For(d.Flavor)
+	selectBuilder := userSessionClientStruct.SelectFrom("user_session_clients")
+	selectBuilder.Where(selectBuilder.In("id", sqlbuilder.Flatten(userSessionClientIds)...))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userSessionClient models.UserSessionClient
+		addr := userSessionClientStruct.Addr(&userSessionClient)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan userSessionClient")
+		}
+		userSessionClients = append(userSessionClients, userSessionClient)
+	}
+
+	return
+}
+
+func (d *CommonDB) GetUserSessionClientsByUserSessionIds(tx *sql.Tx, userSessionIds []int64) (userSessionClients []models.UserSessionClient, err error) {
+	if len(userSessionIds) == 0 {
+		return nil, nil
+	}
+
+	userSessionClientStruct := sqlbuilder.NewStruct(new(models.UserSessionClient)).For(d.Flavor)
+	selectBuilder := userSessionClientStruct.SelectFrom("user_session_clients")
+	selectBuilder.Where(selectBuilder.In("user_session_id", sqlbuilder.Flatten(userSessionIds)...))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userSessionClient models.UserSessionClient
+		addr := userSessionClientStruct.Addr(&userSessionClient)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan userSessionClient")
+		}
+		userSessionClients = append(userSessionClients, userSessionClient)
+	}
+
+	return
+}
