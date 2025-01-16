5ec3c9d0bdf03af1c377102b01f823a468e24fe5
pchchv <ipchchv@gmail.com>
2025-01-16 11:40:50 +0300

    Implement a GetAllGroups and GetAllGroupsPaginated methods

--- src/data/commondb/group.go
+++ src/data/commondb/group.go
@@ -70,6 +70,78 @@ func (d *CommonDB) GetGroupById(tx *sql.Tx, groupId int64) (*models.Group, error
 	return d.getGroupCommon(tx, selectBuilder, groupStruct)
 }
 
+func (d *CommonDB) GetAllGroups(tx *sql.Tx) (groups []models.Group, err error) {
+	groupStruct := sqlbuilder.NewStruct(new(models.Group)).For(d.Flavor)
+	selectBuilder := groupStruct.SelectFrom(d.Flavor.Quote("groups"))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var group models.Group
+		addr := groupStruct.Addr(&group)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan group")
+		}
+
+		groups = append(groups, group)
+	}
+
+	return
+}
+
+func (d *CommonDB) GetAllGroupsPaginated(tx *sql.Tx, page int, pageSize int) (groups []models.Group, total int, err error) {
+	if page < 1 {
+		page = 1
+	}
+
+	if pageSize < 1 {
+		pageSize = 10
+	}
+
+	groupStruct := sqlbuilder.NewStruct(new(models.Group)).For(d.Flavor)
+	selectBuilder := groupStruct.SelectFrom(d.Flavor.Quote("groups"))
+	selectBuilder.OrderBy("group_identifier").Asc()
+	selectBuilder.Offset((page - 1) * pageSize)
+	selectBuilder.Limit(pageSize)
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, 0, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var group models.Group
+		addr := groupStruct.Addr(&group)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, 0, errors.Wrap(err, "unable to scan group")
+		}
+
+		groups = append(groups, group)
+	}
+
+	selectBuilder = d.Flavor.NewSelectBuilder()
+	selectBuilder.Select("count(*)").From(d.Flavor.Quote("groups"))
+	sql, args = selectBuilder.Build()
+	rows2, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, 0, errors.Wrap(err, "unable to query database")
+	}
+	defer rows2.Close()
+
+	if rows2.Next() {
+		if err = rows2.Scan(&total); err != nil {
+			return nil, 0, errors.Wrap(err, "unable to scan count")
+		}
+	}
+
+	return
+}
+
 func (d *CommonDB) getGroupCommon(tx *sql.Tx, selectBuilder *sqlbuilder.SelectBuilder, groupStruct *sqlbuilder.Struct) (*models.Group, error) {
 	sql, args := selectBuilder.Build()
 	rows, err := d.QuerySql(tx, sql, args...)
