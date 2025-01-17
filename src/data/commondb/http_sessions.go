c79eace730ae0e0fbaa8440932c1c8ab5a76c81a
pchchv <ipchchv@gmail.com>
2025-01-17 10:46:14 +0300

    DeleteHttpSession and DeleteHttpSessionExpired methods

--- src/data/commondb/http_sessions.go
+++ src/data/commondb/http_sessions.go
@@ -2,6 +2,7 @@ package commondb
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/huandu/go-sqlbuilder"
 	"github.com/pchchv/aas/src/models"
@@ -13,6 +14,30 @@ func (d *CommonDB) GetHttpSessionById(tx *sql.Tx, httpSessionId int64) (*models.
 	selectBuilder := httpSessionStruct.SelectFrom("http_sessions")
 	selectBuilder.Where(selectBuilder.Equal("id", httpSessionId))
 	return d.getHttpSessionCommon(tx, selectBuilder, httpSessionStruct)
+}
+
+func (d *CommonDB) DeleteHttpSession(tx *sql.Tx, httpSessionId int64) error {
+	userConsentStruct := sqlbuilder.NewStruct(new(models.HttpSession)).For(d.Flavor)
+	deleteBuilder := userConsentStruct.DeleteFrom("http_sessions")
+	deleteBuilder.Where(deleteBuilder.Equal("id", httpSessionId))
+	sql, args := deleteBuilder.Build()
+	if _, err := d.ExecSql(tx, sql, args...); err != nil {
+		return errors.Wrap(err, "unable to delete httpSession")
+	}
+
+	return nil
+}
+
+func (d *CommonDB) DeleteHttpSessionExpired(tx *sql.Tx) error {
+	userConsentStruct := sqlbuilder.NewStruct(new(models.HttpSession)).For(d.Flavor)
+	deleteBuilder := userConsentStruct.DeleteFrom("http_sessions")
+	deleteBuilder.Where(deleteBuilder.LessThan("expires_on", time.Now().UTC()))
+	sql, args := deleteBuilder.Build()
+	if _, err := d.ExecSql(tx, sql, args...); err != nil {
+		return errors.Wrap(err, "unable to delete expired http sessions")
+	}
+
+	return nil
 }
 
 func (d *CommonDB) getHttpSessionCommon(tx *sql.Tx, selectBuilder *sqlbuilder.SelectBuilder, httpSessionStruct *sqlbuilder.Struct) (*models.HttpSession, error) {
