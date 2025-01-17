8a2bc73ab6745616bdf40d7c8a835c704507dd55
pchchv <ipchchv@gmail.com>
2025-01-17 10:09:31 +0300

    GetGroupPermissionsByGroupId&GetGroupPermissionsByGroupIds methods

--- src/data/commondb/group_permission.go
+++ src/data/commondb/group_permission.go
@@ -43,3 +43,54 @@ func (d *CommonDB) CreateGroupPermission(tx *sql.Tx, groupPermission *models.Gro
 	groupPermission.Id = id
 	return nil
 }
+
+func (d *CommonDB) GetGroupPermissionsByGroupId(tx *sql.Tx, groupId int64) (groupPermissions []models.GroupPermission, err error) {
+	groupPermissionStruct := sqlbuilder.NewStruct(new(models.GroupPermission)).For(d.Flavor)
+	selectBuilder := groupPermissionStruct.SelectFrom("groups_permissions")
+	selectBuilder.Where(selectBuilder.Equal("group_id", groupId))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var groupPermission models.GroupPermission
+		addr := groupPermissionStruct.Addr(&groupPermission)
+		if err = rows.Scan(addr...); err != nil {
+			return nil, errors.Wrap(err, "unable to scan groupPermission")
+		}
+		groupPermissions = append(groupPermissions, groupPermission)
+	}
+
+	return
+}
+
+func (d *CommonDB) GetGroupPermissionsByGroupIds(tx *sql.Tx, groupIds []int64) (groupPermissions []models.GroupPermission, err error) {
+	if len(groupIds) == 0 {
+		return nil, nil
+	}
+
+	groupPermissionStruct := sqlbuilder.NewStruct(new(models.GroupPermission)).For(d.Flavor)
+	selectBuilder := groupPermissionStruct.SelectFrom("groups_permissions")
+	selectBuilder.Where(selectBuilder.In("group_id", sqlbuilder.Flatten(groupIds)...))
+	sql, args := selectBuilder.Build()
+	rows, err := d.QuerySql(tx, sql, args...)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to query database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var groupPermission models.GroupPermission
+		addr := groupPermissionStruct.Addr(&groupPermission)
+		err = rows.Scan(addr...)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to scan groupPermission")
+		}
+		groupPermissions = append(groupPermissions, groupPermission)
+	}
+
+	return
+}
