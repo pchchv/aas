677e93b77bb23e5830e86027725b242d5a365c3a
pchchv <ipchchv@gmail.com>
2025-01-18 09:35:54 +0300

    Implement a GetClientById and a GetClientsByIds methods

--- src/data/mysql/client.go
+++ src/data/mysql/client.go
@@ -13,3 +13,11 @@ func (d *MySQLDB) CreateClient(tx *sql.Tx, client *models.Client) error {
 func (d *MySQLDB) UpdateClient(tx *sql.Tx, client *models.Client) error {
 	return d.CommonDB.UpdateClient(tx, client)
 }
+
+func (d *MySQLDB) GetClientById(tx *sql.Tx, clientId int64) (*models.Client, error) {
+	return d.CommonDB.GetClientById(tx, clientId)
+}
+
+func (d *MySQLDB) GetClientsByIds(tx *sql.Tx, clientIds []int64) ([]models.Client, error) {
+	return d.CommonDB.GetClientsByIds(tx, clientIds)
+}
