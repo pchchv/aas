1be6af3094e02df8f57512985b061608497518a5
pchchv <ipchchv@gmail.com>
2025-01-18 09:37:51 +0300

    Implement a methods ClientLoadRedirectURIs and ClientLoadWebOrigins

--- src/data/mysql/client.go
+++ src/data/mysql/client.go
@@ -33,3 +33,11 @@ func (d *MySQLDB) GetClientByClientIdentifier(tx *sql.Tx, clientIdentifier strin
 func (d *MySQLDB) DeleteClient(tx *sql.Tx, clientId int64) error {
 	return d.CommonDB.DeleteClient(tx, clientId)
 }
+
+func (d *MySQLDB) ClientLoadRedirectURIs(tx *sql.Tx, client *models.Client) error {
+	return d.CommonDB.ClientLoadRedirectURIs(tx, client)
+}
+
+func (d *MySQLDB) ClientLoadWebOrigins(tx *sql.Tx, client *models.Client) error {
+	return d.CommonDB.ClientLoadWebOrigins(tx, client)
+}
