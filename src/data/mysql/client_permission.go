8d1c26d10a69d184a83df6c05237e0d2f3bfadc1
pchchv <ipchchv@gmail.com>
2025-01-18 09:40:04 +0300

    GetClientPermissionById, GetClientPermissionByClientIdAndPermissionId and GetClientPermissionsByClientId methods

--- src/data/mysql/client_permission.go
+++ src/data/mysql/client_permission.go
@@ -14,6 +14,18 @@ func (d *MySQLDB) UpdateClientPermission(tx *sql.Tx, clientPermission *models.Cl
 	return d.CommonDB.UpdateClientPermission(tx, clientPermission)
 }
 
+func (d *MySQLDB) GetClientPermissionById(tx *sql.Tx, clientPermissionId int64) (*models.ClientPermission, error) {
+	return d.CommonDB.GetClientPermissionById(tx, clientPermissionId)
+}
+
+func (d *MySQLDB) GetClientPermissionByClientIdAndPermissionId(tx *sql.Tx, clientId, permissionId int64) (*models.ClientPermission, error) {
+	return d.CommonDB.GetClientPermissionByClientIdAndPermissionId(tx, clientId, permissionId)
+}
+
+func (d *MySQLDB) GetClientPermissionsByClientId(tx *sql.Tx, clientId int64) ([]models.ClientPermission, error) {
+	return d.CommonDB.GetClientPermissionsByClientId(tx, clientId)
+}
+
 func (d *MySQLDB) DeleteClientPermission(tx *sql.Tx, clientPermissionId int64) error {
 	return d.CommonDB.DeleteClientPermission(tx, clientPermissionId)
 }
