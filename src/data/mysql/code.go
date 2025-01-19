4c36161dba94fadbc71bca7568ff37d206acc910
pchchv <ipchchv@gmail.com>
2025-01-19 13:22:10 +0300

    Implamen a GetCodeById and a GetCodeByCodeHash methods

--- src/data/mysql/code.go
+++ src/data/mysql/code.go
@@ -13,3 +13,11 @@ func (d *MySQLDB) CreateCode(tx *sql.Tx, code *models.Code) error {
 func (d *MySQLDB) UpdateCode(tx *sql.Tx, code *models.Code) error {
 	return d.CommonDB.UpdateCode(tx, code)
 }
+
+func (d *MySQLDB) GetCodeById(tx *sql.Tx, codeId int64) (*models.Code, error) {
+	return d.CommonDB.GetCodeById(tx, codeId)
+}
+
+func (d *MySQLDB) GetCodeByCodeHash(tx *sql.Tx, codeHash string, used bool) (*models.Code, error) {
+	return d.CommonDB.GetCodeByCodeHash(tx, codeHash, used)
+}
