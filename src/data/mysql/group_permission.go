59af7ef3734f715f11c0fe97e9f097c1e9278928
pchchv <ipchchv@gmail.com>
2025-01-20 09:19:29 +0300

    CreateGroupPermission, UpdateGroupPermission and DeleteGroupPermission methods

--- src/data/mysql/group_permission.go
+++ src/data/mysql/group_permission.go
@@ -6,6 +6,14 @@ import (
 	"github.com/pchchv/aas/src/models"
 )
 
+func (d *MySQLDB) CreateGroupPermission(tx *sql.Tx, groupPermission *models.GroupPermission) error {
+	return d.CommonDB.CreateGroupPermission(tx, groupPermission)
+}
+
+func (d *MySQLDB) UpdateGroupPermission(tx *sql.Tx, groupPermission *models.GroupPermission) error {
+	return d.CommonDB.UpdateGroupPermission(tx, groupPermission)
+}
+
 func (d *MySQLDB) GetGroupPermissionsByGroupId(tx *sql.Tx, groupId int64) ([]models.GroupPermission, error) {
 	return d.CommonDB.GetGroupPermissionsByGroupId(tx, groupId)
 }
@@ -20,4 +28,8 @@ func (d *MySQLDB) GetGroupPermissionById(tx *sql.Tx, groupPermissionId int64) (*
 
 func (d *MySQLDB) GetGroupPermissionByGroupIdAndPermissionId(tx *sql.Tx, groupId, permissionId int64) (*models.GroupPermission, error) {
 	return d.CommonDB.GetGroupPermissionByGroupIdAndPermissionId(tx, groupId, permissionId)
+}
+
+func (d *MySQLDB) DeleteGroupPermission(tx *sql.Tx, groupPermissionId int64) error {
+	return d.CommonDB.DeleteGroupPermission(tx, groupPermissionId)
 }
