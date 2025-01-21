71879c7b42986931d4c05fa4c9490c260c288850
pchchv <ipchchv@gmail.com>
2025-01-21 10:39:48 +0300

    Implement DecodeAndValidateTokenString method to TokenParser for JWT validation

--- src/oauth/token_parser.go
+++ src/oauth/token_parser.go
@@ -30,3 +30,33 @@ func (tp *TokenParser) getPublicKey() (pubKey *rsa.PublicKey, err error) {
 
 	return
 }
+
+func (tp *TokenParser) DecodeAndValidateTokenString(token string, pubKey *rsa.PublicKey, withExpirationCheck bool) (t *Jwt, err error) {
+	if pubKey == nil {
+		if pubKey, err = tp.getPublicKey(); err != nil {
+			return nil, err
+		}
+	}
+
+	t = &Jwt{
+		TokenBase64: token,
+	}
+
+	if len(token) > 0 {
+		claims, opts := jwt.MapClaims{}, []jwt.ParserOption{}
+		if withExpirationCheck {
+			opts = append(opts, jwt.WithExpirationRequired())
+		} else {
+			opts = append(opts, jwt.WithoutClaimsValidation())
+		}
+
+		if _, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+			return pubKey, nil
+		}, opts...); err != nil {
+			return nil, err
+		}
+		t.Claims = claims
+	}
+
+	return
+}
