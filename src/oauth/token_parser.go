b8466c20fc9b8d66907ee0882a2bbf1cec4140e5
pchchv <ipchchv@gmail.com>
2025-01-21 10:20:30 +0300

    Implement public key retrieval method to TokenParser for JWT handling

--- src/oauth/token_parser.go
+++ src/oauth/token_parser.go
@@ -1,6 +1,11 @@
 package oauth
 
-import db "github.com/pchchv/aas/src/data"
+import (
+	"crypto/rsa"
+
+	"github.com/golang-jwt/jwt"
+	db "github.com/pchchv/aas/src/data"
+)
 
 type TokenParser struct {
 	database db.Database
@@ -10,4 +15,18 @@ func NewTokenParser(database db.Database) *TokenParser {
 	return &TokenParser{
 		database: database,
 	}
+}
+
+func (tp *TokenParser) getPublicKey() (pubKey *rsa.PublicKey, err error) {
+	keyPair, err := tp.database.GetCurrentSigningKey(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKey, err = jwt.ParseRSAPublicKeyFromPEM(keyPair.PublicKeyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	return
 }
