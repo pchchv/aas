37d1fc5c29191584c88fbee0e98fba553bd0810c
pchchv <ipchchv@gmail.com>
2025-01-21 11:57:24 +0300

    Add TokenIssuer struct and constructor for managing token issuance

--- src/oauth/token_issuer.go
+++ src/oauth/token_issuer.go
@@ -1,10 +1,25 @@
 package oauth
 
-import "github.com/pchchv/aas/src/models"
+import (
+	"github.com/pchchv/aas/src/database"
+	"github.com/pchchv/aas/src/models"
+)
 
 type GenerateTokenForRefreshInput struct {
 	Code             *models.Code
 	RefreshToken     *models.RefreshToken
 	RefreshTokenInfo *Jwt
 	ScopeRequested   string
+}
+
+type TokenIssuer struct {
+	database    database.Database
+	tokenParser *TokenParser
+}
+
+func NewTokenIssuer(database database.Database, tokenParser *TokenParser) *TokenIssuer {
+	return &TokenIssuer{
+		database:    database,
+		tokenParser: tokenParser,
+	}
 }
