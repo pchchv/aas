350c96eba54af2bc03d815698fd1bc2859b2c144
pchchv <ipchchv@gmail.com>
2025-01-22 11:17:09 +0300

    Add context keys for JWT information and session management

---
+++ src/constants/context_key.go
@@ -0,0 +1,8 @@
+package constants
+
+type ctxKey string
+
+const ContextKeyJwtInfo ctxKey = "JwtInfo"
+const ContextKeySettings ctxKey = "Settings"
+const ContextKeyBearerToken ctxKey = "BearerToken"
+const ContextKeySessionIdentifier ctxKey = "SessionIdentifier"
