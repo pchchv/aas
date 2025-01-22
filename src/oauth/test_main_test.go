969c68aca8bb6e1ea593308504d3c52d6ac04b61
pchchv <ipchchv@gmail.com>
2025-01-22 11:51:03 +0300

    Add TestMain function to initialize configuration for OAuth tests

---
+++ src/oauth/test_main_test.go
@@ -0,0 +1,14 @@
+package oauth
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pchchv/aas/src/config"
+)
+
+func TestMain(m *testing.M) {
+	config.Init("AuthServer")
+	code := m.Run()
+	os.Exit(code)
+}
