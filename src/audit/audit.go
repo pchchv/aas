ebca209d12b32d97d66ea49cc0b11da72b5d20e8
pchchv <ipchchv@gmail.com>
2025-01-23 10:45:13 +0300

    Add AuditLogger type and constructor for audit logging

--- src/audit/audit.go
+++ src/audit/audit.go
@@ -4,3 +4,10 @@ type AuditEvent struct {
 	AuditEvent string                 `json:"audit_event"`
 	Details    map[string]interface{} `json:"details"`
 }
+
+type AuditLogger struct {
+}
+
+func NewAuditLogger() *AuditLogger {
+	return &AuditLogger{}
+}
