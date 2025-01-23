b18f319a1f2b1236ddfc4de9655e3cc6ceb61829
pchchv <ipchchv@gmail.com>
2025-01-23 10:25:06 +0300

    Add CodeIssuer type and constructor for database integration in OAuth

--- src/oauth/code_issuer.go
+++ src/oauth/code_issuer.go
@@ -1,6 +1,18 @@
 package oauth
 
+import "github.com/pchchv/aas/src/database"
+
 type CreateCodeInput struct {
 	AuthContext
 	SessionIdentifier string
+}
+
+type CodeIssuer struct {
+	database database.Database
+}
+
+func NewCodeIssuer(db database.Database) *CodeIssuer {
+	return &CodeIssuer{
+		database: db,
+	}
 }
