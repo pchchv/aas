e208e4ae4f9dcbdc5214b86d60976db757d3e9ca
pchchv <ipchchv@gmail.com>
2025-01-23 10:46:23 +0300

    Implement logging functionality in AuditLogger for console output

--- src/audit/audit.go
+++ src/audit/audit.go
@@ -1,5 +1,12 @@
 package audit
 
+import (
+	"encoding/json"
+	"log/slog"
+
+	"github.com/pchchv/aas/src/config"
+)
+
 type AuditEvent struct {
 	AuditEvent string                 `json:"audit_event"`
 	Details    map[string]interface{} `json:"details"`
@@ -10,4 +17,22 @@ type AuditLogger struct {
 
 func NewAuditLogger() *AuditLogger {
 	return &AuditLogger{}
+}
+
+func (al *AuditLogger) Log(auditEvent string, details map[string]interface{}) {
+	if !config.Get().AuditLogsInConsole {
+		return
+	}
+
+	evt := AuditEvent{
+		AuditEvent: auditEvent,
+		Details:    details,
+	}
+
+	if eventJSON, err := json.Marshal(evt); err != nil {
+		slog.Error("failed to marshal audit event", "error", err, "event", auditEvent)
+		return
+	} else {
+		slog.Info(string(eventJSON))
+	}
 }
