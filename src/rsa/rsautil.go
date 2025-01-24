360350d75ddbbe285dde502669db39a84e021754
pchchv <ipchchv@gmail.com>
2025-01-24 11:05:44 +0300

    Implement functions to encode RSA private and public keys to PEM format

--- src/rsa/rsautil.go
+++ src/rsa/rsautil.go
@@ -3,6 +3,8 @@ package rsautil
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 )
 
 func GeneratePrivateKey(bitSize int) (privateKey *rsa.PrivateKey, err error) {
@@ -15,4 +17,33 @@ func GeneratePrivateKey(bitSize int) (privateKey *rsa.PrivateKey, err error) {
 	}
 
 	return privateKey, nil
+}
+
+func EncodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+	// ASN.1 DER format
+	privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+	// pem.Block
+	privBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privDER,
+	}
+
+	return pem.EncodeToMemory(&privBlock)
+}
+
+func EncodePublicKeyToPEM(publicKey *rsa.PublicKey) ([]byte, error) {
+	pubkey_bytes, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	pubkey_pem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: pubkey_bytes,
+		},
+	)
+
+	return pubkey_pem, nil
 }
