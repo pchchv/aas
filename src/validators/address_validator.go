4c9bb0ff3d541641b97dcbfc3a7a1beec6b4b374
pchchv <ipchchv@gmail.com>
2025-01-25 19:54:39 +0300

    Add ValidateAddressInput type

---
+++ src/validators/address_validator.go
@@ -0,0 +1,10 @@
+package validators
+
+type ValidateAddressInput struct {
+	AddressLine1      string
+	AddressLine2      string
+	AddressLocality   string
+	AddressRegion     string
+	AddressPostalCode string
+	AddressCountry    string
+}
