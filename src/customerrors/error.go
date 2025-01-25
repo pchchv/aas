8c234d212b261896e3f5ef09749d562e9d72447f
pchchv <ipchchv@gmail.com>
2025-01-25 19:58:54 +0300

    Add constructors for ErrorDetail with optional HTTP status code

--- src/customerrors/error.go
+++ src/customerrors/error.go
@@ -1,5 +1,28 @@
 package customerrors
+import "fmt"
 
 type ErrorDetail struct {
 	details map[string]string
+}
+
+func NewErrorDetail(code string, description string) *ErrorDetail {
+	details := make(map[string]string)
+	details["code"] = code
+	details["description"] = description
+	return &ErrorDetail{
+		details: details,
+	}
+}
+
+func NewErrorDetailWithHttpStatusCode(code string, description string, httpStatusCode int) *ErrorDetail {
+	details := make(map[string]string)
+	details["code"] = code
+	details["description"] = description
+	if httpStatusCode >= 100 && httpStatusCode < 600 {
+		details["httpStatusCode"] = fmt.Sprint(httpStatusCode)
+	}
+
+	return &ErrorDetail{
+		details: details,
+	}
 }
