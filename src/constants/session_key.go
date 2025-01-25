39fef1363cb51f92f8610442290a31364e319e30
pchchv <ipchchv@gmail.com>
2025-01-25 09:07:20 +0300

    Add session key constants for session management

---
+++ src/constants/session_key.go
@@ -0,0 +1,13 @@
+package constants
+
+const SessionName string = "AAS"
+const SessionKeySessionIdentifier string = "SessionIdentifier"
+const SessionKeyOTPImage string = "OTPImage"
+const SessionKeyOTPSecret string = "OTPSecret"
+const SessionKeyAuthContext string = "AuthContext"
+const SessionKeyJwt string = "Jwt"
+const SessionKeyState string = "State"
+const SessionKeyNonce string = "Nonce"
+const SessionKeyRedirectURI string = "RedirectURI"
+const SessionKeyCodeVerifier string = "CodeVerifier"
+const SessionKeyRedirectBack string = "RedirectBack"
