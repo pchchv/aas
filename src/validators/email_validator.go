d7857d5ad717d09a0b7ac928118d683b99987ef1
pchchv <ipchchv@gmail.com>
2025-01-26 13:51:29 +0300

    Add EmailValidator type and constructor for email validation

--- src/validators/email_validator.go
+++ src/validators/email_validator.go
@@ -1,7 +1,19 @@
 package validators
 
+import "github.com/pchchv/aas/src/database"
+
 type ValidateEmailInput struct {
 	Email             string
 	EmailConfirmation string
 	Subject           string
+}
+
+type EmailValidator struct {
+	database database.Database
+}
+
+func NewEmailValidator(database database.Database) *EmailValidator {
+	return &EmailValidator{
+		database: database,
+	}
 }
