1e496d15b0dd0a669b2320d9de19eacb99e9758c
pchchv <ipchchv@gmail.com>
2025-01-26 13:54:13 +0300

    Implement email validation logic in EmailValidator

--- src/validators/email_validator.go
+++ src/validators/email_validator.go
@@ -1,6 +1,11 @@
 package validators
 
-import "github.com/pchchv/aas/src/database"
+import (
+	"regexp"
+
+	"github.com/pchchv/aas/src/customerrors"
+	"github.com/pchchv/aas/src/database"
+)
 
 type ValidateEmailInput struct {
 	Email             string
@@ -16,4 +21,29 @@ func NewEmailValidator(database database.Database) *EmailValidator {
 	return &EmailValidator{
 		database: database,
 	}
+}
+
+func (val *EmailValidator) ValidateEmail(emailAddress string) error {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$` // basic regex pattern for email validation
+	regex, err := regexp.Compile(pattern)
+	if err != nil {
+		return err
+	}
+
+	if !regex.MatchString(emailAddress) {
+		return customerrors.NewErrorDetail("", "Please enter a valid email address.")
+	}
+
+	atIndex := regexp.MustCompile("@").FindStringIndex(emailAddress)
+	localPart := emailAddress[:atIndex[0]]
+
+	if regexp.MustCompile(`\.\.`).MatchString(emailAddress) {
+		return customerrors.NewErrorDetail("", "Please enter a valid email address.")
+	}
+
+	if localPart[0] == '.' || localPart[len(localPart)-1] == '.' {
+		return customerrors.NewErrorDetail("", "Please enter a valid email address.")
+	}
+
+	return nil
 }
