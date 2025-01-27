9598e509e872996769630666710d44b0079d24d7
pchchv <ipchchv@gmail.com>
2025-01-27 10:07:47 +0300

    Add TokenParser and AuditLogger interfaces for token validation and auditing

--- src/validators/token_validator.go
+++ src/validators/token_validator.go
@@ -1,5 +1,19 @@
 package validators
 
+import (
+	"crypto/rsa"
+
+	"github.com/pchchv/aas/src/oauth"
+)
+
 type PermissionChecker interface {
 	UserHasScopePermission(userId int64, scope string) (bool, error)
+}
+
+type TokenParser interface {
+	DecodeAndValidateTokenString(token string, pubKey *rsa.PublicKey, withExpirationCheck bool) (*oauth.Jwt, error)
+}
+
+type AuditLogger interface {
+	Log(auditEvent string, details map[string]interface{})
 }
