f7392868967dbc35cfde9c11490a2f56aea84a82
pchchv <ipchchv@gmail.com>
2025-01-27 10:14:31 +0300

    Implement validateClientCredentialsScopes method for scope validation in TokenValidator

--- src/validators/token_validator.go
+++ src/validators/token_validator.go
@@ -2,10 +2,16 @@ package validators
 
 import (
 	"crypto/rsa"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strings"
 
+	"github.com/pchchv/aas/src/customerrors"
 	"github.com/pchchv/aas/src/database"
 	"github.com/pchchv/aas/src/models"
 	"github.com/pchchv/aas/src/oauth"
+	"github.com/pchchv/aas/src/oidc"
 )
 
 type PermissionChecker interface {
@@ -54,4 +60,83 @@ func NewTokenValidator(database database.Database, tokenParser TokenParser,
 		permissionChecker: permissionChecker,
 		auditLogger:       auditLogger,
 	}
+}
+
+func (val *TokenValidator) validateClientCredentialsScopes(scope string, client *models.Client) error {
+	if len(scope) == 0 {
+		return nil
+	}
+
+	space := regexp.MustCompile(`\s+`)
+	scope = space.ReplaceAllString(scope, " ")
+	scopes := strings.Split(scope, " ")
+	for _, scopeStr := range scopes {
+		if oidc.IsIdTokenScope(scopeStr) || oidc.IsOfflineAccessScope(scopeStr) {
+			return customerrors.NewErrorDetailWithHttpStatusCode("invalid_request",
+				fmt.Sprintf(
+					"Id token scopes (such as '%v') are not supported in the client credentials flow. Please use scopes in the format 'resource:permission' (e.g., 'backendA:read'). Multiple scopes can be specified, separated by spaces.",
+					scopeStr,
+				),
+				http.StatusBadRequest)
+		}
+
+		parts := strings.Split(scopeStr, ":")
+		if len(parts) != 2 {
+			return customerrors.NewErrorDetailWithHttpStatusCode("invalid_scope",
+				fmt.Sprintf(
+					"Invalid scope format: '%v'. Scopes must adhere to the resource-identifier:permission-identifier format. For instance: backend-service:create-product.",
+					scopeStr,
+				),
+				http.StatusBadRequest)
+		}
+
+		res, err := val.database.GetResourceByResourceIdentifier(nil, parts[0])
+		if err != nil {
+			return err
+		} else if res == nil {
+			return customerrors.NewErrorDetailWithHttpStatusCode("invalid_scope",
+				fmt.Sprintf("Invalid scope: '%v'. Could not find a resource with identifier '%v'.", scopeStr, parts[0]),
+				http.StatusBadRequest)
+		}
+
+		permissions, err := val.database.GetPermissionsByResourceId(nil, res.Id)
+		if err != nil {
+			return err
+		}
+
+		permissionExists := false
+		for _, perm := range permissions {
+			if perm.PermissionIdentifier == parts[1] {
+				permissionExists = true
+				break
+			}
+		}
+
+		if !permissionExists {
+			return customerrors.NewErrorDetailWithHttpStatusCode("invalid_scope",
+				fmt.Sprintf(
+					"Scope '%v' is not recognized. The resource identified by '%v' doesn't grant the '%v' permission.",
+					scopeStr,
+					parts[0],
+					parts[1],
+				),
+				http.StatusBadRequest)
+		}
+
+		clientHasPermission := false
+		for _, perm := range client.Permissions {
+			if perm.PermissionIdentifier == parts[1] {
+				clientHasPermission = true
+				break
+			}
+		}
+
+		if !clientHasPermission {
+			return customerrors.NewErrorDetailWithHttpStatusCode("invalid_scope",
+				fmt.Sprintf("Permission to access scope '%v' is not granted to the client.", scopeStr),
+				http.StatusBadRequest)
+		}
+	}
+
+	return nil
 }
