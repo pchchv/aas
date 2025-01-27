1273ed5a067b4f712bd0a92a3cccbda2a6f5f8c1
pchchv <ipchchv@gmail.com>
2025-01-27 09:53:08 +0300

    Add unit tests for ValidateEmailUpdate method in EmailValidator

--- src/validators/email_validator_test.go
+++ src/validators/email_validator_test.go
@@ -3,9 +3,12 @@ package validators
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/pchchv/aas/src/customerrors"
 	"github.com/pchchv/aas/src/database/mocks"
+	"github.com/pchchv/aas/src/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestValidateEmail(t *testing.T) {
@@ -28,6 +31,91 @@ func TestValidateEmail(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := validator.ValidateEmail(tt.email)
+			if tt.expectedError == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+				customErr, ok := err.(*customerrors.ErrorDetail)
+				assert.True(t, ok)
+				assert.Equal(t, tt.expectedError, customErr.GetDescription())
+			}
+		})
+	}
+}
+
+func TestValidateEmailUpdate(t *testing.T) {
+	mockDB := mocks.NewDatabase(t)
+	validator := NewEmailValidator(mockDB)
+	subject1 := uuid.New()
+	subject2 := uuid.New()
+	tests := []struct {
+		name          string
+		input         ValidateEmailInput
+		mockSetup     func()
+		expectedError string
+	}{
+		{
+			name: "Valid email update",
+			input: ValidateEmailInput{
+				Email:             "new@example.com",
+				EmailConfirmation: "new@example.com",
+				Subject:           subject1.String(),
+			},
+			mockSetup: func() {
+				mockDB.On("GetUserBySubject", mock.Anything, subject1.String()).Return(&models.User{Subject: subject1}, nil)
+				mockDB.On("GetUserByEmail", mock.Anything, "new@example.com").Return(nil, nil)
+			},
+			expectedError: "",
+		},
+		{
+			name: "Empty email",
+			input: ValidateEmailInput{
+				Email:             "",
+				EmailConfirmation: "",
+				Subject:           subject1.String(),
+			},
+			mockSetup:     func() {},
+			expectedError: "Please enter an email address.",
+		},
+		{
+			name: "Email too long",
+			input: ValidateEmailInput{
+				Email:             "thisemailaddressiswaytoolongandexceedsthemaximumlengthof60characters@example.com",
+				EmailConfirmation: "thisemailaddressiswaytoolongandexceedsthemaximumlengthof60characters@example.com",
+				Subject:           subject1.String(),
+			},
+			mockSetup:     func() {},
+			expectedError: "The email address cannot exceed a maximum length of 60 characters.",
+		},
+		{
+			name: "Email mismatch",
+			input: ValidateEmailInput{
+				Email:             "new@example.com",
+				EmailConfirmation: "different@example.com",
+				Subject:           subject1.String(),
+			},
+			mockSetup:     func() {},
+			expectedError: "The email and email confirmation entries must be identical.",
+		},
+		{
+			name: "Email already registered",
+			input: ValidateEmailInput{
+				Email:             "existing@example.com",
+				EmailConfirmation: "existing@example.com",
+				Subject:           subject1.String(),
+			},
+			mockSetup: func() {
+				mockDB.On("GetUserBySubject", mock.Anything, subject1.String()).Return(&models.User{Subject: subject1}, nil)
+				mockDB.On("GetUserByEmail", mock.Anything, "existing@example.com").Return(&models.User{Subject: subject2}, nil)
+			},
+			expectedError: "Apologies, but this email address is already registered.",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mockSetup()
+			err := validator.ValidateEmailUpdate(&tt.input)
 			if tt.expectedError == "" {
 				assert.NoError(t, err)
 			} else {
