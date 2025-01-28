d5cbdef5a152dedfea930e956a88c1742c4d88ff
pchchv <ipchchv@gmail.com>
2025-01-28 11:34:15 +0300

    Add PhoneValidator type and constructor for phone validation

--- src/validators/phone_validator.go
+++ src/validators/phone_validator.go
@@ -1,7 +1,19 @@
 package validators
 
+import "github.com/pchchv/aas/src/database"
+
 type ValidatePhoneInput struct {
 	PhoneNumber          string
 	PhoneNumberVerified  bool
 	PhoneCountryUniqueId string
+}
+
+type PhoneValidator struct {
+	database database.Database
+}
+
+func NewPhoneValidator(database database.Database) *PhoneValidator {
+	return &PhoneValidator{
+		database: database,
+	}
 }
