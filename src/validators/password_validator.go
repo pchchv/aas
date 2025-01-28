a0c62a6c36ea3d62e8c768bda9817b040db56d82
pchchv <ipchchv@gmail.com>
2025-01-28 10:47:32 +0300

    Add methods to PasswordValidator for number and special character checks

--- src/validators/password_validator.go
+++ src/validators/password_validator.go
@@ -1,8 +1,28 @@
 package validators
 
+import "unicode"
+
 type PasswordValidator struct {
 }
 
 func NewPasswordValidator() *PasswordValidator {
 	return &PasswordValidator{}
+}
+
+func (val *PasswordValidator) containsNumber(s string) bool {
+	for _, char := range s {
+		if unicode.IsNumber(char) {
+			return true
+		}
+	}
+	return false
+}
+
+func (val *PasswordValidator) containsSpecialChar(s string) bool {
+	for _, char := range s {
+		if !unicode.IsLetter(char) && !unicode.IsNumber(char) {
+			return true
+		}
+	}
+	return false
 }
