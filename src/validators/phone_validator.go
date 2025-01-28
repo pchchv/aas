651aa59b84238287cfe9283b02bb79425c475d39
pchchv <ipchchv@gmail.com>
2025-01-28 11:35:07 +0300

    Add isSimplePattern function to enhance phone number validation

--- src/validators/phone_validator.go
+++ src/validators/phone_validator.go
@@ -1,6 +1,10 @@
 package validators
 
-import "github.com/pchchv/aas/src/database"
+import (
+	"strings"
+
+	"github.com/pchchv/aas/src/database"
+)
 
 type ValidatePhoneInput struct {
 	PhoneNumber          string
@@ -16,4 +20,21 @@ func NewPhoneValidator(database database.Database) *PhoneValidator {
 	return &PhoneValidator{
 		database: database,
 	}
+}
+
+func isSimplePattern(number string) bool {
+	// Check for all repeated digits (e.g., 00000, 111111111, etc.)
+	if len(number) > 0 && strings.Count(number, string(number[0])) == len(number) {
+		return true
+	}
+
+	// Check for sequential ascending digits
+	ascending := "0123456789"
+	if strings.Contains(ascending, number) {
+		return true
+	}
+
+	// Check for sequential descending digits
+	descending := "9876543210"
+	return strings.Contains(descending, number)
 }
