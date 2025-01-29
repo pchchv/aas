0a20d0ff743a423655ce6ef7ccf95dd6d3a5f6c3
pchchv <ipchchv@gmail.com>
2025-01-29 11:13:17 +0300

    Implement GetAuthContext and GetLoggedInSubject methods to AuthHelper for improved session management

--- src/helpers/auth_helper.go
+++ src/helpers/auth_helper.go
@@ -1,6 +1,16 @@
 package handlerhelpers
 
-import "github.com/gorilla/sessions"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/gorilla/sessions"
+	"github.com/pchchv/aas/src/constants"
+	"github.com/pchchv/aas/src/customerrors"
+	"github.com/pchchv/aas/src/oauth"
+)
 
 type AuthHelper struct {
 	sessionStore sessions.Store
@@ -10,4 +20,34 @@ func NewAuthHelper(sessionStore sessions.Store) *AuthHelper {
 	return &AuthHelper{
 		sessionStore: sessionStore,
 	}
+}
+
+func (s *AuthHelper) GetAuthContext(r *http.Request) (*oauth.AuthContext, error) {
+	sess, err := s.sessionStore.Get(r, constants.SessionName)
+	if err != nil {
+		return nil, err
+	}
+
+	var authContext oauth.AuthContext
+	if jsonData, ok := sess.Values[constants.SessionKeyAuthContext].(string); !ok {
+		return nil, customerrors.ErrNoAuthContext
+	} else if err = json.Unmarshal([]byte(jsonData), &authContext); err != nil {
+		return nil, err
+	}
+
+	return &authContext, nil
+}
+
+func (s *AuthHelper) GetLoggedInSubject(r *http.Request) string {
+	if r.Context().Value(constants.ContextKeyJwtInfo) != nil {
+		if jwtInfo, ok := r.Context().Value(constants.ContextKeyJwtInfo).(oauth.JwtInfo); !ok {
+			stackBytes := debug.Stack()
+			slog.Error("unable to cast jwtInfo\n" + string(stackBytes))
+			return ""
+		} else if jwtInfo.IdToken != nil {
+			return jwtInfo.IdToken.GetStringClaim("sub")
+		}
+	}
+
+	return ""
 }
