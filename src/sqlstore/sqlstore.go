1a87ba44e1c2d633335b840aa1261d962045601b
pchchv <ipchchv@gmail.com>
2025-01-29 10:31:17 +0300

    Add SQLStore type and constructor for session management

---
+++ src/sqlstore/sqlstore.go
@@ -0,0 +1,36 @@
+package sessionstore
+
+import (
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
+	"github.com/pchchv/aas/src/database"
+)
+
+type SQLStore struct {
+	db      database.Database
+	Codecs  []securecookie.Codec
+	Options *sessions.Options
+}
+
+func NewSQLStore(db database.Database, path string, maxAge int, httpOnly bool, secure bool, sameSite http.SameSite, keyPairs ...[]byte) *SQLStore {
+	codecs := securecookie.CodecsFromPairs(keyPairs...)
+	for _, codec := range codecs {
+		if sc, ok := codec.(*securecookie.SecureCookie); ok {
+			sc.MaxLength(1024 * 64) // 64k
+		}
+	}
+
+	return &SQLStore{
+		db:     db,
+		Codecs: codecs,
+		Options: &sessions.Options{
+			Path:     path,
+			MaxAge:   maxAge,
+			HttpOnly: httpOnly,
+			Secure:   secure,
+			SameSite: sameSite,
+		},
+	}
+}
