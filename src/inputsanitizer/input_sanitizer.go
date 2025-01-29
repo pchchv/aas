ddfc6ba5a4b3102ccb42887f12da07858c7c65c3
pchchv <ipchchv@gmail.com>
2025-01-29 11:01:46 +0300

    Implement Sanitize method to InputSanitizer for HTML string sanitization

--- src/inputsanitizer/input_sanitizer.go
+++ src/inputsanitizer/input_sanitizer.go
@@ -1,8 +1,23 @@
 package inputsanitizer
 
+import (
+	"log/slog"
+
+	"github.com/sym01/htmlsanitizer"
+)
+
 type InputSanitizer struct {
 }
 
 func NewInputSanitizer() *InputSanitizer {
 	return &InputSanitizer{}
+}
+
+func (i *InputSanitizer) Sanitize(str string) string {
+	if sanitizedHTML, err := htmlsanitizer.SanitizeString(str); err != nil {
+		slog.Error("unable to sanitize string: " + err.Error())
+		return str
+	} else {
+		return sanitizedHTML
+	}
 }
