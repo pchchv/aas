958a7cd6f4541c94b94efa4c1d6d939f21c9de61
pchchv <ipchchv@gmail.com>
2025-01-30 10:30:44 +0300

    Add unit tests for RenderTemplate method in HttpHelper

--- src/helpers/http_helper_test.go
+++ src/helpers/http_helper_test.go
@@ -3,11 +3,12 @@ package helpers
 import (
 	"bytes"
 	"context"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/pchchv/aas/src/constants"
-	mocksData "github.com/pchchv/aas/src/data/mocks"
+	mocksData "github.com/pchchv/aas/src/database/mocks"
 	"github.com/pchchv/aas/src/mocks"
 	"github.com/pchchv/aas/src/models"
 	"github.com/pchchv/aas/src/oauth"
@@ -65,6 +66,56 @@ func TestRenderTemplateToBuffer(t *testing.T) {
 		assert.Contains(t, buf.String(), "Hello, JohnDoe!")
 
 		mockDatabase.AssertExpectations(t)
+	})
+}
+
+func TestRenderTemplate(t *testing.T) {
+	templateFS := &mocks.TestFS{
+		FileContents: map[string]string{
+			"layouts/layout.html": "<html>{{template \"content\" .}}</html>",
+			"page.html":           "{{define \"content\"}}Hello, {{.Name}}! Status: {{._httpStatus}}{{end}}",
+		},
+	}
+	database := mocksData.NewDatabase(t)
+	httpHelper := NewHttpHelper(templateFS, database)
+
+	t.Run("Without _httpStatus", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		ctx := req.Context()
+		ctx = context.WithValue(ctx, constants.ContextKeySettings, &models.Settings{AppName: "TestApp", UITheme: "light"})
+		req = req.WithContext(ctx)
+		data := map[string]interface{}{
+			"Name": "John",
+		}
+
+		err := httpHelper.RenderTemplate(w, req, "layouts/layout.html", "page.html", data)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "text/html; charset=UTF-8", w.Header().Get("Content-Type"))
+		assert.Contains(t, w.Body.String(), "Hello, John!")
+		assert.Contains(t, w.Body.String(), "Status:")
+		assert.Equal(t, http.StatusOK, w.Code) // Default status should be 200 OK
+	})
+
+	t.Run("With _httpStatus", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/", nil)
+		w := httptest.NewRecorder()
+		ctx := req.Context()
+		ctx = context.WithValue(ctx, constants.ContextKeySettings, &models.Settings{AppName: "TestApp", UITheme: "light"})
+		req = req.WithContext(ctx)
+		data := map[string]interface{}{
+			"Name":        "Jane",
+			"_httpStatus": http.StatusCreated,
+		}
+
+		err := httpHelper.RenderTemplate(w, req, "layouts/layout.html", "page.html", data)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "text/html; charset=UTF-8", w.Header().Get("Content-Type"))
+		assert.Contains(t, w.Body.String(), "Hello, Jane!")
+		assert.Contains(t, w.Body.String(), "Status: 201")
+		assert.Equal(t, http.StatusCreated, w.Code)
 	})
 }
 
