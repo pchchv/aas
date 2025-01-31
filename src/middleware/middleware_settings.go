53367cc1f0135eabb4d5e04f493e129ac128320f
pchchv <ipchchv@gmail.com>
2025-01-31 10:07:01 +0300

    Add MiddlewareSettings function for database settings retrieval in middleware

---
+++ src/middleware/middleware_settings.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pchchv/aas/src/constants"
+	"github.com/pchchv/aas/src/database"
+)
+
+func MiddlewareSettings(database database.Database) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			if settings, err := database.GetSettingsById(nil, 1); err != nil {
+				slog.Error(fmt.Sprintf("%+v\nrequest-id: %v", err, middleware.GetReqID(r.Context())))
+				http.Error(w, fmt.Sprintf("fatal failure in GetSettings() middleware. For additional information, refer to the server logs. Request Id: %v", middleware.GetReqID(r.Context())), http.StatusInternalServerError)
+			} else {
+				ctx = context.WithValue(ctx, constants.ContextKeySettings, settings)
+				next.ServeHTTP(w, r.WithContext(ctx))
+			}
+		}
+		return http.HandlerFunc(fn)
+	}
+}
