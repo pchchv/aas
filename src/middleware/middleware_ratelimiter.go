8598f430e4d240b9fbc4434855e437fd42074608
pchchv <ipchchv@gmail.com>
2025-01-31 10:15:19 +0300

    Add RateLimiterMiddleware to manage request rate limits for various actions

--- src/middleware/middleware_ratelimiter.go
+++ src/middleware/middleware_ratelimiter.go
@@ -2,10 +2,30 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/go-chi/httprate"
 	"github.com/pchchv/aas/src/oauth"
 )
 
 type AuthHelper interface {
 	GetAuthContext(r *http.Request) (*oauth.AuthContext, error)
+}
+
+type RateLimiterMiddleware struct {
+	authHelper      AuthHelper
+	pwdLimiter      *httprate.RateLimiter
+	otpLimiter      *httprate.RateLimiter
+	activateLimiter *httprate.RateLimiter
+	resetPwdLimiter *httprate.RateLimiter
+}
+
+func NewRateLimiterMiddleware(authHelper AuthHelper) *RateLimiterMiddleware {
+	return &RateLimiterMiddleware{
+		authHelper:      authHelper,
+		pwdLimiter:      httprate.NewRateLimiter(10, 1*time.Minute),
+		otpLimiter:      httprate.NewRateLimiter(10, 1*time.Minute),
+		activateLimiter: httprate.NewRateLimiter(5, 5*time.Minute),
+		resetPwdLimiter: httprate.NewRateLimiter(5, 5*time.Minute),
+	}
 }
