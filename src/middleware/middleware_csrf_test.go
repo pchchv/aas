9a53d6d2b58ae8de43bd5805bd2689ff3e4bc3ac
pchchv <ipchchv@gmail.com>
2025-01-31 10:19:31 +0300

    Add unit tests for CSRF middleware skip checks

---
+++ src/middleware/middleware_csrf_test.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const skipCheckKey string = "gorilla.csrf.Skip"
+
+func TestMiddlewareSkipCsrf(t *testing.T) {
+	tests := []struct {
+		name string
+		path string
+		skip bool
+	}{
+		{"Static path", "/static/file.css", true},
+		{"Userinfo path", "/userinfo", true},
+		{"Token path", "/auth/token", true},
+		{"Callback path", "/auth/callback", true},
+		{"Other path", "/other", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", tt.path, nil)
+			rr := httptest.NewRecorder()
+			handler := MiddlewareSkipCsrf()(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				skipValue := r.Context().Value(skipCheckKey)
+				if tt.skip {
+					if skipValue != true {
+						t.Errorf("MiddlewareSkipCsrf() for path %s: expected CSRF check to be skipped, but it wasn't", tt.path)
+					}
+				} else {
+					if skipValue == true {
+						t.Errorf("MiddlewareSkipCsrf() for path %s: expected CSRF check not to be skipped, but it was", tt.path)
+					}
+				}
+			}))
+
+			handler.ServeHTTP(rr, req)
+		})
+	}
+}
