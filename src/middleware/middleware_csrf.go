0e7840aa40245b76847f280a05d8fbaa9fcac3bd
pchchv <ipchchv@gmail.com>
2025-01-31 10:18:11 +0300

    Add CSRF middleware to skip checks for specific routes

---
+++ src/middleware/middleware_csrf.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/csrf"
+)
+
+func MiddlewareSkipCsrf() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			skip := false
+			if strings.HasPrefix(r.URL.Path, "/static") ||
+				strings.HasPrefix(r.URL.Path, "/userinfo") ||
+				strings.HasPrefix(r.URL.Path, "/auth/token") ||
+				strings.HasPrefix(r.URL.Path, "/auth/callback") {
+				skip = true
+			}
+
+			if skip {
+				r = csrf.UnsafeSkipCheck(r)
+			}
+
+			next.ServeHTTP(w, r.WithContext(r.Context()))
+		}
+		return http.HandlerFunc(fn)
+	}
+}
