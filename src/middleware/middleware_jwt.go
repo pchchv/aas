71d987ddc3d248a27c89212f284f1a91984026dd
pchchv <ipchchv@gmail.com>
2025-02-01 09:34:58 +0300

    Add authHelper interface for authorization and authentication handling

--- src/middleware/middleware_jwt.go
+++ src/middleware/middleware_jwt.go
@@ -15,3 +15,9 @@ type tokenParser interface {
 	DecodeAndValidateTokenResponse(tokenResponse *oauth.TokenResponse) (*oauth.JwtInfo, error)
 	DecodeAndValidateTokenString(token string, pubKey *rsa.PublicKey, withExpirationCheck bool) (*oauth.Jwt, error)
 }
+
+type authHelper interface {
+	RedirToAuthorize(w http.ResponseWriter, r *http.Request, clientIdentifier string, scope string, redirectBack string) error
+	IsAuthorizedToAccessResource(jwtInfo oauth.JwtInfo, scopesAnyOf []string) bool
+	IsAuthenticated(jwtInfo oauth.JwtInfo) bool
+}
