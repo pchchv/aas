ad41c52c6d7ad0877d3c7dfa69c4500f24f10151
pchchv <ipchchv@gmail.com>
2025-02-01 09:34:15 +0300

    Add tokenParser interface for JWT handling in middleware

--- src/middleware/middleware_jwt.go
+++ src/middleware/middleware_jwt.go
@@ -1,7 +1,17 @@
 package middleware
 
-import "net/http"
+import (
+	"crypto/rsa"
+	"net/http"
+
+	"github.com/pchchv/aas/src/oauth"
+)
 
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
+}
+
+type tokenParser interface {
+	DecodeAndValidateTokenResponse(tokenResponse *oauth.TokenResponse) (*oauth.JwtInfo, error)
+	DecodeAndValidateTokenString(token string, pubKey *rsa.PublicKey, withExpirationCheck bool) (*oauth.Jwt, error)
 }
