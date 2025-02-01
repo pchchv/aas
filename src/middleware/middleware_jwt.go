02b2fba45649b81ed6b36ca4cbae95fde6db9983
pchchv <ipchchv@gmail.com>
2025-02-01 09:36:18 +0300

    Add MiddlewareJwt type and constructor for JWT handling

--- src/middleware/middleware_jwt.go
+++ src/middleware/middleware_jwt.go
@@ -4,11 +4,21 @@ import (
 	"crypto/rsa"
 	"net/http"
 
+	"github.com/gorilla/sessions"
+	"github.com/pchchv/aas/src/database"
 	"github.com/pchchv/aas/src/oauth"
 )
 
 type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
+}
+
+type MiddlewareJwt struct {
+	sessionStore sessions.Store
+	tokenParser  tokenParser
+	database     database.Database
+	authHelper   authHelper
+	httpClient   HTTPClient
 }
 
 type tokenParser interface {
@@ -20,4 +30,14 @@ type authHelper interface {
 	RedirToAuthorize(w http.ResponseWriter, r *http.Request, clientIdentifier string, scope string, redirectBack string) error
 	IsAuthorizedToAccessResource(jwtInfo oauth.JwtInfo, scopesAnyOf []string) bool
 	IsAuthenticated(jwtInfo oauth.JwtInfo) bool
+}
+
+func NewMiddlewareJwt(sessionStore sessions.Store, tokenParser tokenParser, database database.Database, authHelper authHelper, httpClient HTTPClient) *MiddlewareJwt {
+	return &MiddlewareJwt{
+		sessionStore: sessionStore,
+		tokenParser:  tokenParser,
+		database:     database,
+		authHelper:   authHelper,
+		httpClient:   httpClient,
+	}
 }
