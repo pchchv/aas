6a927e12d252447fe4d13e150f65fadd6ee1bfb7
pchchv <ipchchv@gmail.com>
2025-02-01 09:45:59 +0300

    Add unit tests for JwtAuthorizationHeaderToContext middleware

---
+++ src/middleware/middleware_jwt_test.go
@@ -0,0 +1,105 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/pchchv/aas/src/constants"
+	dataMocks "github.com/pchchv/aas/src/database/mocks"
+	helpersMocks "github.com/pchchv/aas/src/helpers/mocks"
+	"github.com/pchchv/aas/src/oauth"
+	OAuthMocks "github.com/pchchv/aas/src/oauth/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestJwtAuthorizationHeaderToContext_ValidBearerToken(t *testing.T) {
+	mockTokenParser := new(OAuthMocks.TokenParser)
+	mockDatabase := new(dataMocks.Database)
+	mockAuthHelper := new(helpersMocks.AuthHelper)
+	middleware := NewMiddlewareJwt(nil, mockTokenParser, mockDatabase, mockAuthHelper, nil)
+	expectedToken := &oauth.Jwt{
+		TokenBase64: "validtoken",
+		Claims: map[string]interface{}{
+			"sub": "user",
+		},
+	}
+	mockTokenParser.On("DecodeAndValidateTokenString", "validtoken", mock.Anything, true).
+		Return(expectedToken, nil)
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Authorization", "Bearer validtoken")
+	rr := httptest.NewRecorder()
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Context().Value(constants.ContextKeyBearerToken)
+		assert.NotNil(t, token)
+		assert.IsType(t, oauth.Jwt{}, token)
+		assert.Equal(t, "validtoken", token.(oauth.Jwt).TokenBase64)
+		assert.Equal(t, "user", token.(oauth.Jwt).Claims["sub"])
+	})
+
+	handler := middleware.JwtAuthorizationHeaderToContext()(nextHandler)
+	handler.ServeHTTP(rr, req)
+
+	mockTokenParser.AssertExpectations(t)
+}
+
+func TestJwtAuthorizationHeaderToContext_InvalidBearerToken(t *testing.T) {
+	mockTokenParser := new(OAuthMocks.TokenParser)
+	mockDatabase := new(dataMocks.Database)
+	mockAuthHelper := new(helpersMocks.AuthHelper)
+	middleware := NewMiddlewareJwt(nil, mockTokenParser, mockDatabase, mockAuthHelper, nil)
+	mockTokenParser.On("DecodeAndValidateTokenString", "invalidtoken", mock.Anything, true).
+		Return(nil, assert.AnError)
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Authorization", "Bearer invalidtoken")
+	rr := httptest.NewRecorder()
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Context().Value(constants.ContextKeyBearerToken)
+		assert.Nil(t, token)
+	})
+
+	handler := middleware.JwtAuthorizationHeaderToContext()(nextHandler)
+	handler.ServeHTTP(rr, req)
+
+	mockTokenParser.AssertExpectations(t)
+}
+
+func TestJwtAuthorizationHeaderToContext_NoBearerToken(t *testing.T) {
+	mockTokenParser := new(OAuthMocks.TokenParser)
+	mockDatabase := new(dataMocks.Database)
+	mockAuthHelper := new(helpersMocks.AuthHelper)
+	middleware := NewMiddlewareJwt(nil, mockTokenParser, mockDatabase, mockAuthHelper, nil)
+	req := httptest.NewRequest("GET", "/", nil)
+	rr := httptest.NewRecorder()
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Context().Value(constants.ContextKeyBearerToken)
+		assert.Nil(t, token)
+	})
+
+	handler := middleware.JwtAuthorizationHeaderToContext()(nextHandler)
+	handler.ServeHTTP(rr, req)
+}
+
+func TestJwtAuthorizationHeaderToContext_InvalidAuthorizationHeader(t *testing.T) {
+	mockTokenParser := new(OAuthMocks.TokenParser)
+	mockDatabase := new(dataMocks.Database)
+	mockAuthHelper := new(helpersMocks.AuthHelper)
+	middleware := NewMiddlewareJwt(nil, mockTokenParser, mockDatabase, mockAuthHelper, nil)
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("Authorization", "NotBearer token")
+	rr := httptest.NewRecorder()
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Context().Value(constants.ContextKeyBearerToken)
+		assert.Nil(t, token)
+	})
+
+	handler := middleware.JwtAuthorizationHeaderToContext()(nextHandler)
+	handler.ServeHTTP(rr, req)
+}
